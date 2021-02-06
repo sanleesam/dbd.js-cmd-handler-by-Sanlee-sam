f42a6c7b7cead927f393e91e015bc3b8b2eed01a
sanleesam <76587514+sanleesam@users.noreply.github.com>
2021-02-06 20:31:49 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+const dbd = require("dbd.js")
+ 
+const bot = new dbd.Bot({
+token: "TOKEN", 
+prefix: "!" 
+})
+ 
+bot.onMessage()
+ 
+const fs = require('fs')
+
+const folders = fs.readdirSync("./commands/")
+
+for (const files of folders) {
+const folder = fs.readdirSync(`./commands/${files}/`).filter(file => file.endsWith(".js"))
+
+for (const commands of folder) {
+const command = require(`./commands/${files}/${commands}`) 
+bot.command({
+name: command.name,
+code: command.code
+})
+} 
+}
